3da73efd1eb639fcee200d5ff951030085836306
dav7445 <random.email.random.001@gmail.com>
2021-11-24 09:29:52 +0100

    WinrateCalculator

---
+++ WinrateCalculator/script.js
@@ -0,0 +1,25 @@
+function win_rate_model(eval, ply) {
+	const PawnValueEg = 208;
+	const v = eval * PawnValueEg;
+	let m = Math.min(240, ply) / 64;
+	let as = [-3.68389304,  30.07065921, -60.52878723, 149.53378557];
+	let bs = [-2.0181857,   15.85685038, -29.83452023,  47.59078827];
+	let a = (((as[0] * m + as[1]) * m + as[2]) * m) + as[3];
+	let b = (((bs[0] * m + bs[1]) * m + bs[2]) * m) + bs[3];
+	var x = Math.min(Math.max(((100 * v) / PawnValueEg), -2000), 2000);
+	return Math.round(0.5 + 1000 / (1 + Math.exp((a - x) / b)));
+}
+
+function calculateWinRate() {
+	var eval = document.inputs.eval.value * 1;
+	var ply = document.inputs.move.value * 2;
+	let wdl_w = win_rate_model(eval, ply);
+	let wdl_l = win_rate_model(-eval, ply);
+	let wdl_d = 1000 - wdl_w - wdl_l;
+	let wdl_w_perc = (wdl_w / 10)+"%";
+	let wdl_l_perc = (wdl_l / 10)+"%";
+	let wdl_d_perc = (wdl_d / 10)+"%";
+	document.result.win.value = wdl_w_perc;
+	document.result.loss.value = wdl_l_perc;
+	document.result.draw.value = wdl_d_perc;
+}
