ba45c1b56f28629e209dc7bf427373803428a9db
dav <63931154+dav1312@users.noreply.github.com>
2022-04-15 20:02:26 +0200

    Update WDL Model

--- WinrateCalculator/script.js
+++ WinrateCalculator/script.js
@@ -2,8 +2,8 @@ function win_rate_model(eval, ply) {
 	const PawnValueEg = 208;
 	const v = eval * PawnValueEg;
 	let m = Math.min(240, ply) / 64;
-	let as = [-3.68389304,  30.07065921, -60.52878723, 149.53378557];
-	let bs = [-2.0181857,   15.85685038, -29.83452023,  47.59078827];
+	let as = [-1.17202460e-01, 5.94729104e-01, 1.12065546e+01, 1.22606222e+02];
+	let bs = [-1.79066759, 11.30759193, -17.43677612, 36.47147479];
 	let a = (((as[0] * m + as[1]) * m + as[2]) * m) + as[3];
 	let b = (((bs[0] * m + bs[1]) * m + bs[2]) * m) + bs[3];
 	var x = Math.min(Math.max(((100 * v) / PawnValueEg), -2000), 2000);
